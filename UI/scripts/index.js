f342325d241822255c196191c767ef5f79b70aa1
Thiernope <ntirandth@gmail.com>
2020-11-02 09:04:22 +0200

    fix(fix-bug):send query -facilitate user to send query [deliver]

---
+++ UI/scripts/index.js
@@ -0,0 +1,29 @@
+//add a query
+const url = "https://desolate-ridge-00597.herokuapp.com/api/queries";
+const form = document.getElementById("query-form");
+form.addEventListener('submit',(e)=>{ 
+
+    const firstName = document.getElementById("first-name");
+    const lastName = document.getElementById("last-name");
+    const email = document.getElementById("email");
+    const message= document.getElementById("message");
+    e.preventDefault();
+    fetch(url,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        message: message.value
+        })
+    })
+    .then(res => res.json())
+    .then(data =>{
+        alert(data.message);
+    })
+
+    form.reset();
+})
